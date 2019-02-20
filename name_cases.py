f2e77608ffd2b729ed9ddd241ab40dd63c76a83e
Michael Ocasio <michael.a.ocasiofigueroa@gmail.com>
2019-02-20 13:30:36 -0500

    addition: name_cases.py
    - Worked with certian aspects of pythoon such as .upper() .lower() and title() among discipling my title naming more.

---
+++ name_cases.py
@@ -0,0 +1,30 @@
+# 2.3
+
+ruth_Name = "Ruth Ocasio"
+learn_Python = "would you like to learn Python today?"
+greeting = "Hey " + ruth_Name + ", " + learn_Python
+print(greeting)
+
+# 2.4
+
+print(ruth_Name.lower())
+print(ruth_Name.upper())
+print(ruth_Name.title())
+
+# 2.5
+
+famous_author = "Albert Einstein"
+quote = '"A person who never made a mistake never tried anything new"'
+sentence = famous_author + "once said, " + quote
+print(sentence)
+
+
+# 2.7
+
+my_name = " 'Michael Ocasio' "
+print(my_name)
+my_name = my_name.rstrip()
+print(my_name)
+my_name = my_name.lstrip()
+print(my_name)
+
